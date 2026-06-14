c004817ccafcb1f4d415a9ba5a6ba801d220b184
infinitedim <dragdimas9@gmail.com>
2026-06-14 21:48:26 +0700

    init

---
+++ packages/just_ui_cli/bin/just_ui_cli.dart
@@ -0,0 +1,5 @@
+import 'package:just_ui_cli/just_ui_cli.dart' as just_ui_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${just_ui_cli.calculate()}!');
+}
---
+++ packages/just_ui_cli/test/just_ui_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:just_ui_cli/just_ui_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
