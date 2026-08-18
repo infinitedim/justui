2300fce932025e4b06f5876221d4bab0b2f41b78
dimas <dragdimas9@gmail.com>
2026-08-18 11:16:38 +0700

    remove all registry (resetting conflict)

--- registry/components/accordion/neobrutalism/just_accordion_variants.dart
+++
@@ -1,8 +0,0 @@
-/// The visual style variants for [JustAccordion].
-enum JustAccordionVariant {
-  /// Default flush list with separator lines between items.
-  default_,
-
-  /// Contained within a single outer border container, items separated by lines.
-  contained,
-}
