b7a6d3b0a309dec9b73f3ea5562b29e93f768604
dimas <dragdimas9@gmail.com>
2026-09-13 13:57:02 +0700

    fix e2e test

--- apps/docs/playwright.config.ts
+++ apps/docs/playwright.config.ts
@@ -18,12 +18,10 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
   ],
-  webServer: process.env.CI
-    ? {
-        command: 'bun run start',
-        url: 'http://localhost:3000',
-        reuseExistingServer: false,
-        timeout: 120_000,
-      }
-    : undefined,
+  webServer: {
+    command: 'bun run start',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120_000,
+  },
 });
